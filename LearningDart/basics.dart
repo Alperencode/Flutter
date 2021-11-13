d21c8b86a7c1c0bc776b1522471b6fff0a4f5001
Alperen Aga <alperencode@gmail.com>
2021-11-13 16:29:40 +0300

    Update basics.dart :wrench:

--- LearningDart/basics.dart
+++ LearningDart/basics.dart
@@ -5,6 +5,7 @@ void main(){
   print('Hello World');
 
   // Variable Types
+  print("\nVariable Section");
   dynamic name = "alperen";
   name = 10;
   String stringVar = "Hello";    // String
@@ -21,8 +22,9 @@ void main(){
   // int _number = int.parse(_name);  // Converted string into integer 
   //_name = _number.toString();     // Converted integer into string 
 
-
-  // Operations and If-else
+  print("------------------------------");
+  // Operations and if-else
+  print("\nOperations and if-else Section");
   var number1 = 20;
   var number2 = 15;
   var age = 17;
@@ -42,7 +44,9 @@ void main(){
     print("Person cannot vote");
   }
   
+  print("------------------------------");
   // Switch-case
+  print("\nSwitch-case Section");
   var weather = "day";
 
   switch(weather){
@@ -58,16 +62,21 @@ void main(){
       print("Idk");
   }
 
+  print("------------------------------");
   // Random
+  print("\nRandom Section");
   Random randomInt = new Random();  // Random
   print("Random: ${randomInt.nextInt(100)}");   // declaring the max value with nextInt(max)
 
+  print("------------------------------");
   // Strings
+  print("\nString Section");
   String multiLine ="""I
   am
   Alperen""";
   print(multiLine);
   String s1 = "Hello World";
+
   /*
   s1.toUpperCase
   s1.toLowerCase
@@ -78,6 +87,56 @@ void main(){
   s1.replaceAll(RegExp("World"),"Everyone")
   s1.split(",")
   s1.trim()
-   
   */
+
+  print("------------------------------");
+  // Lists
+  print("\nList Section");
+  var l1 = <String>[];                                // Creating empty list
+  List<String> l2 = ["Apple","Banana","Lemon"];      // Defining and creating list
+  var l3 = [];                                      // Creating a dynamic list
+  var sentence = l2.join(" ");                     // Turning list into string
+  for(var i in l2){
+    print(i);
+  }
+  print(sentence);
+   
+  var l4 = [12,21,2,3,4,124,12];
+  for(var i = 0; i < l4.length; i++){
+      print(l4[i]);
+  }
+
+  print("------------------------------");
+  // Functions
+  print("\nFunction Section");
+  int SumNumbers(x,y){
+      return x+y;
+  }
+  int DecNumbers(x,y){
+      return x-y;
+  }
+  String Greeting(name){
+      return "\nHello ${name}!";
+  }
+  void instantGreeting(){
+      print("Hi!!");
+  }
+
+  print(Greeting("Alperen"));
+  instantGreeting(); 
+  print("------------------------------");
+
+  // Map (Python Dict)
+  print("\nMap Section\n");
+  Map<String, int> map1 = new Map<String, int>();
+  var hero = {
+    "Iron Man":"Tony Start",
+    "Age":48,
+    "Male":"Male",
+  };
+  hero["Super Power"] = "Ironman Suit";
+  hero.addAll({"Wife":"Pepper Potts"});
+  hero.keys.forEach((specials) {print("Keys: ${specials}");}); 
+  hero.values.forEach((values) {print("Values: ${values}");}); 
+  
 }
