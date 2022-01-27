f945e07cef0c875fa56eda055610d6ffc91ca6c2
Alperen Aga <alperencode@gmail.com>
2022-01-27 17:57:25 +0300

    Add practice.dart

---
+++ LearningDart/practice.dart
@@ -0,0 +1,13 @@
+// I'm deleting the lines after I'm done using it so this file will be short,simple and have only last part of my practice
+void main(List<String> args) {
+  String s1;
+
+  s1 = "Alperen Aga 210205058";
+  List<String> l1 = s1.split(" ");
+  for (var item in l1) {
+
+    print(item);
+    
+  }
+}
+
