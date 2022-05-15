2da573e59a2cc343b21d786276e27289ee4bfa97
Alperen Aga <alperencode@gmail.com>
2022-05-15 12:13:47 +0300

    Update ui_challenge

--- ui_challenge/lib/ImageCarousel.dart
+++ ui_challenge/lib/ImageCarousel.dart
@@ -1,4 +1,4 @@
-import 'package:carousel_slider/carousel_slider.dart';
+// import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class ImageCarousel extends StatelessWidget {
@@ -9,83 +9,9 @@ class ImageCarousel extends StatelessWidget {
         title: const Text("GFG Slider"),
       ),
       body: ListView(
-        children: [
-          CarouselSlider(
-            items: [
-              //1st Image of Slider
-              Container(
-                margin: const EdgeInsets.all(6.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  image: const DecorationImage(
-                    image: NetworkImage(
-                        "https://wallpaperaccess.com/full/2637581.jpg"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-
-              //2nd Image of Slider
-              Container(
-                margin: const EdgeInsets.all(6.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  image: const DecorationImage(
-                    image: NetworkImage("ADD IMAGE URL HERE"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-
-              //3rd Image of Slider
-              Container(
-                margin: const EdgeInsets.all(6.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  image: const DecorationImage(
-                    image: NetworkImage("ADD IMAGE URL HERE"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-
-              //4th Image of Slider
-              Container(
-                margin: const EdgeInsets.all(6.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  image: const DecorationImage(
-                    image: NetworkImage("ADD IMAGE URL HERE"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-
-              //5th Image of Slider
-              Container(
-                margin: const EdgeInsets.all(6.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  image: const DecorationImage(
-                    image: NetworkImage("ADD IMAGE URL HERE"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ],
-
-            //Slider Container properties
-            options: CarouselOptions(
-              height: 180.0,
-              enlargeCenterPage: true,
-              autoPlay: true,
-              aspectRatio: 16 / 9,
-              autoPlayCurve: Curves.fastOutSlowIn,
-              enableInfiniteScroll: true,
-              autoPlayAnimationDuration: const Duration(milliseconds: 800),
-              viewportFraction: 0.8,
-            ),
-          ),
+        children: const [
+          // This did not work
+          // I'll implement Carousel later
         ],
       ),
     );
